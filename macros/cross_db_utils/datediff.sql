df8244083a381d0d313df3c8b45687d460af7026
HasanJ <hasan_sg@hotmail.com>
2020-02-13 10:52:26 +0200

    Update datediff.sql

--- macros/cross_db_utils/datediff.sql
+++ macros/cross_db_utils/datediff.sql
@@ -1,5 +1,40 @@
 {% macro datediff(first_date, second_date, datepart) %}
-  {{ adapter_macro('dbt_utils.datediff', first_date, second_date, datepart) }}
+    {% if datepart == 'year' %}
+
+        DATE_PART('year', {{ second_date }}) - DATE_PART('year', {{ first_date }})
+
+    {% elif datepart == 'month' %}
+
+        ( DATE_PART('year', {{ second_date }}) - DATE_PART('year', {{ first_date }}) ) * 12
+            + DATE_PART('month', {{ second_date }}) - DATE_PART('month', {{ first_date }})
+
+    {% elif datepart == 'day' %}
+
+        {{ second_date }} - {{ first_date }}
+
+    {% elif datepart == 'week' %}
+
+        FLOOR ( ({{ second_date }} - {{ first_date }}) / 7 )
+
+    {% elif datepart == 'hour' %}
+
+        ( {{ second_date }} - {{ first_date }} ) * 24
+            + DATE_PART('hour', {{ second_date }}) - DATE_PART('hour', {{ first_date }})
+
+    {% elif datepart == 'minute' %}
+
+        ( ( {{ second_date }} - {{ first_date }} ) * 24
+            + DATE_PART('hour', {{ second_date }}) - DATE_PART('hour', {{ first_date }}) ) * 60
+                + DATE_PART('minute', {{ second_date }}) - DATE_PART('minute', {{ first_date }})
+
+    {% elif datepart == 'second' %}
+
+        ( ( ( {{ second_date }} - {{ first_date }} ) * 24
+            + DATE_PART('hour', {{ second_date }}) - DATE_PART('hour', {{ first_date }}) ) * 60
+                + DATE_PART('minute', {{ second_date }}) - DATE_PART('minute', {{ first_date }}) ) * 60
+                    + DATE_PART('second', {{ second_date }}) - DATE_PART('second', {{ first_date }})
+
+    {% endif %}
 {% endmacro %}
 
 
